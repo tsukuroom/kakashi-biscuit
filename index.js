8ed5c301a133f5946c8c40229a87e085de2ad5d0
Yoshiyuki Toda <ytoda129@gmail.com>
2016-07-18 23:55:38 +0900

    thetaモジュール実装中

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 'use strict';
 
+const theta = require('./lib/theta');
+// const imageProcessor = require('lib/imageprocessor');
+// const birdRecognizer = require('lib/birdrecognizer');
+
+theta.capture()
+  .then(() => {
+    return theta.downloadLatest();
+  })
+  .catch(() => {
+    console.log('error');
+  });
+
+// theta.capture()
+//   .then(() => {
+//     return theta.downloadLatest();
+//   })
+//   .then((path) => {
+//     return imageProc.trimming();
+//   })
+//   .then((pathList) => {
+//     return birdRec.recognize();
+//   })
+//   .then((result) => {
+//     // 鳥がいたらarduinoに指示を出す。鳥がいなければ何もしない
+//   })
+//   .catch(() => {
+
+//   });
