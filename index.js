400ef7d4f443632aa8354781c0a75cc9672d44d3
Yoshiyuki Toda <ytoda129@gmail.com>
2016-07-22 00:29:22 +0900

    Arduino側で近づいたことを認識して、鳥がいたらクラッカー引っ張るところまで ただし、シリアルポートの指定はMac用

--- index.js
+++ index.js
@@ -5,32 +5,37 @@ const debug = require('debug')('index');
 const theta = require('./lib/theta');
 const imageProcessor = require('./lib/imageprocessor');
 const birdRecognizer = require('./lib/birdrecognizer');
+const SerialPort = require('serialport');
 
 const imageDir = './images';
 
-theta.captureImageAndDownload()
-  .then((image) => {
-    debug('capture success image=' + image.name);
-    return writeImage(image, imageDir);
-  })
-  .then((path) => {
-    debug('path=' + path);
-    return imageProcessor.trimming(path);
-  })
-  .then((croppedFilePath) => {
-    debug('croppedFilePath=' + croppedFilePath);
-    return birdRecognizer.birdExists(croppedFilePath);
-  })
-  .then((exist) => {
-    if (exist) {
-      debug('bird exists');
-    } else {
-      debug('bird not exists');
-    }
-  })
-  .catch(() => {
-    debug('capture fail');
-  });
+function processBirdDetect() {
+  theta.captureImageAndDownload()
+    .then((image) => {
+      debug('capture success image=' + image.name);
+      return writeImage(image, imageDir);
+    })
+    .then((path) => {
+      debug('path=' + path);
+      return imageProcessor.trimming(path);
+    })
+    .then((croppedFilePath) => {
+      debug('croppedFilePath=' + croppedFilePath);
+      return birdRecognizer.birdExists(croppedFilePath);
+    })
+    .then((exist) => {
+      if (exist) {
+        debug('bird exists');
+        port.write('cracker\n');
+      } else {
+        debug('bird not exists');
+      }
+      ignoreFromArduino = false;
+    })
+    .catch(() => {
+      debug('capture fail');
+    });
+}
 
 function writeImage(image, dir) {
   return new Promise((resolve, reject) => {
@@ -44,7 +49,8 @@ function writeImage(image, dir) {
                  (error) => {
                    if (error) {
                      debug(error);
-                     return reject(error);
+                     reject(error);
+                     return;
                    }
                    debug(`written file: ${filepath}`);
                    resolve(filepath);
@@ -52,19 +58,45 @@ function writeImage(image, dir) {
   });
 }
 
-// theta.capture()
-//   .then(() => {
-//     return theta.downloadLatest();
-//   })
-//   .then((path) => {
-//     return imageProc.trimming();
-//   })
-//   .then((pathList) => {
-//     return birdRec.recognize();
-//   })
-//   .then((result) => {
-//     // 鳥がいたらarduinoに指示を出す。鳥がいなければ何もしない
-//   })
-//   .catch(() => {
+var serialopts = {
+  baudRate: 9600,
+  dataBits: 8,
+  parity: 'none',
+  stopBits: 1,
+  flowControl: false,
+  parser: SerialPort.parsers.readline("\n")
+};
 
-//   });
+const port = new SerialPort('/dev/cu.usbmodem1411', serialopts);
+
+var ignoreFromArduino = false;
+
+port.on('open', (err) => {
+  if (err) {
+    debug(err);
+    return;
+  }
+
+  setTimeout(() => {
+    port.write('start 50\n', (err) => {
+      if (err) {
+        debug(err);
+        return;
+      }
+
+      debug('written');
+    }, 2000);
+  });
+});
+
+port.on('data', (input) => {
+  if (ignoreFromArduino) {
+    return;
+  }
+  ignoreFromArduino = true;
+  processBirdDetect();
+});
+
+port.on('err', (err) => {
+  debug(err);
+});
