713a93a0aef5aa5423566923230005cfafee3299
Yoshiyuki Toda <ytoda129@gmail.com>
2016-07-26 16:56:51 +0900

    トリミング後画像の品質を80に変更。 サウンドデバイスの番号を動的に決定するように修正。

--- shell/startup.sh
+++ shell/startup.sh
@@ -5,12 +5,14 @@ KAKASHI_HOME=$(cd $(dirname $0); pwd)/..
 mkdir -p $KAKASHI_HOME/images
 mkdir -p $KAKASHI_HOME/log
 
+CARDNO=$(aplay -l | grep USB | awk '{print sub(/.$/,"",$2)}')
+
 export AUDIODRIVER=alsa
-export AUDIODEV=plughw:1,0
+export AUDIODEV=plughw:${CARDNO:-0},0
 
 export CLARIFAI_ID=<clarifai_id>
 export CLARIFAI_SECRET=<clarifai_secret>
 
-cd $KAKAKSHI_HOME
-DEBUG=index,theta,birdrecognizer /usr/bin/node . >>$KAKASHI_HOME/log/kakashi.log 2>&1 &
+cd $KAKASHI_HOME
+DEBUG=index,theta,imageprocessor,birdrecognizer /usr/bin/node . >>$KAKASHI_HOME/log/kakashi.log 2>&1 &
 
